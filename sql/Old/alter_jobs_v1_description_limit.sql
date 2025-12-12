41c5b15c4d1088293dc14c02bf5720e4bf2d71ea
graemeboulton <email@graemeboulton.com>
2025-12-12 09:15:53 +0000

    file tidy up

--- sql/Old/alter_jobs_v1_description_limit.sql
+++
@@ -1,4 +0,0 @@
--- Add a character length constraint to job_description
-ALTER TABLE staging.jobs_v1
-    ADD CONSTRAINT jobs_v1_job_description_len
-    CHECK (char_length(job_description) <= 20000);
